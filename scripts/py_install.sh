55a1fb3b7bb9f57d5c0dc33d23fe55bff462a5b3
cartwatson <cartwatson@gmail.com>
2023-11-09 16:03:15 -0700

    update to work with unix

--- scripts/py_install.sh
+++ scripts/py_install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# catch errors
+trap 'echo "An error occurred. Exiting..."; deactivate; exit 1;' ERR
+set -e
+
 echo "Setting up virual environment..."
-python -m venv .venv
+python -m venv .venv || python3 -m venv .venv
 
 echo "Activating venv..."
 source .venv/Scripts/activate || source .venv/bin/activate
 
 echo "Updating pip and installing requirements..."
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install --upgrade pip || python3 -m pip install --upgrade pip
+pip install -r scripts/requirements.txt
 deactivate
